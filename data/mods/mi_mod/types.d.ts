3a832faf54c26f855ad71571a84cc8b16a7eb619
Juand <ejemplo0127@gmail.com>
2025-11-19 01:15:47 +0100

    VFinal

--- data/mods/mi_mod/types.d.ts
+++ data/mods/mi_mod/types.d.ts
@@ -1,5 +1,18 @@
-import type { AbilityData, MoveData, SpeciesData } from "../../.sim-dist/types";
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+import type {
+	AbilityData,
+	MoveData,
+	SpeciesData,
+	ConditionData,
+	FormatList,
+} from "../../.sim-dist/types";
+import type { Pokemon } from "../../.sim-dist/types";
 
+/** Tipos extendidos para tu mod */
 export interface ModdedAbilityData extends AbilityData {}
 export interface ModdedMoveData extends MoveData {}
 export interface ModdedSpeciesData extends SpeciesData {}
+export interface ModdedConditionData extends ConditionData {}
+export type ModdedPokemonSet = Pokemon;
+export type ModdedFormatList = FormatList;
+/* eslint-enable @typescript-eslint/no-empty-object-type */
