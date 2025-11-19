50af3e00585ca97ada4f22f555a87c973fe204e1
Juand <ejemplo0127@gmail.com>
2025-11-19 01:11:07 +0100

    VFinal

---
+++ data/mods/mi_mod/types.d.ts
@@ -0,0 +1,5 @@
+import type { AbilityData, MoveData, SpeciesData } from "../../.sim-dist/types";
+
+export interface ModdedAbilityData extends AbilityData {}
+export interface ModdedMoveData extends MoveData {}
+export interface ModdedSpeciesData extends SpeciesData {}
